6b9cb88bd4ffaf82bc20b0fc6384c9913e9b040b
guhenriofc <you@example.com>
2021-01-17 14:45:46 -0200

    Projeto finalizado

--- Controllers/EquipeController.cs
+++ Controllers/EquipeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Projeto_MVC_E_Players.Models;
@@ -34,12 +35,58 @@ namespace Projeto_MVC_E_Players.Controllers
             Equipe novaEquipe   = new Equipe();
             novaEquipe.IdEquipe = Int32.Parse( form["IdEquipe"] );
             novaEquipe.Nome     = form["Nome"];
-            novaEquipe.Imagem   = form["Imagem"];
+            
+
+            //Upload Inicio
+            //Verificamos se o usuario anexou um arquivo
+            if ( form.Files.Count > 0 )
+            {
+                //Se sim
+                //Armazenamos o arquivona variavel file
+                var file    = form.Files[0];
+                var folder  = Path.Combine( Directory.GetCurrentDirectory(), "wwwroot/img/Equipes" );
+
+                //Verificamos se a pasta Equipes nao existe
+                if (!Directory.Exists(folder))
+                {
+                    //Se nao existir a pasta, a criamos 
+                    Directory.CreateDirectory(folder);
+                }
+
+                                            //localhost:5001    +                       + Equipes + equipe.jpg
+                var path = Path.Combine( Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName );
+
+                using ( var stream = new FileStream(path, FileMode.Create))
+                {
+                    //Salvamos o arquivo no caminho definido
+                    file.CopyTo(stream);
+                }
+
+                novaEquipe.Imagem = file.FileName;
+
+            }
+            else
+            {
+                novaEquipe.Imagem = "padrao.png";
+            }
+            //upload Termino
 
             //Chamando o metodo Create para salvar
             //a novaEquipe no CSV
             equipeModel.Create(novaEquipe);
             //Listar e armazer na ViewBag
+            ViewBag.Equipes = equipeModel.ReadAll();
+
+
+            return LocalRedirect("~/Equipe/Listar");
+        }
+
+        //http://localhost:5001/Equipe/1
+        [Route("{id}")]
+        public IActionResult Excluir(int id)
+        {
+            equipeModel.Delete(id);
+            //istando as exquipes apos a exclusao
             ViewBag.Equipes = equipeModel.ReadAll();
 
 
